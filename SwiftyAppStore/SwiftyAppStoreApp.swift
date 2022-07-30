c7bcb32f01ed8361e1d4d7d4b4e0b4cb96608f1c
Mico Infante <kinfante@isbx.com>
2022-07-30 16:03:45 +0800

    Initial Commit

---
+++ SwiftyAppStore/SwiftyAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyAppStoreApp.swift
+//  SwiftyAppStore
+//
+//  Created by Mico Infante on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
