4f9c4c868227bb2b7dc7d8a2179250c60c57d0b9
Mico Infante <kinfante@isbx.com>
2022-07-31 17:23:59 +0800

    Conform to `APICall` to add endpoints

--- SwiftyAppStore/Repositories/HomeRepository.swift
+++ SwiftyAppStore/Repositories/HomeRepository.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 
-protocol HomeRepositoryProtocol: AnyObject {
+// Repository is an overkill but for demo purposes ...
+protocol HomeRepositoryProtocol: NetworkManagerProtocol {
     func fetchGames(completion: ([Game]) -> Void)
 //    func getGameExtraDetail(by id: String, completion: (Game) -> Void)
 }
 
 final class HomeRepository: HomeRepositoryProtocol {
+    var session: URLSession = URLSession.shared
+    var baseURL: String = ""
+    var bgQueue: DispatchQueue = DispatchQueue(label: "bg_parse_queue")
+
     private let apiService: HomeAPIServiceProtocol
 
     init(apiService: HomeAPIServiceProtocol = HomeAPIService()) {
@@ -20,11 +25,46 @@ final class HomeRepository: HomeRepositoryProtocol {
     }
 
     func fetchGames(completion: ([Game]) -> Void) {
-        apiService.fetchGames(completion: completion)
+
     }
 
     func getGameExtraDetail(by id: String, completion: (Game) -> Void) {
-//        apiService.getExtraDetails(id: id, completion: completion)
+        
     }
 
+}
+
+// Service Endpoints
+extension HomeRepository {
+    enum API {
+        case allGames
+        case gameDetails(String)
+    }
+}
+
+// Actual endpoints
+extension HomeRepository.API: APICall {
+    var path: String {
+        switch self {
+        case .allGames:
+            return "/"
+        case let .gameDetails(id):
+            return "/game/\(id)"
+        }
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .allGames, .gameDetails: return .get
+        }
+    }
+
+    var headers: [String : String]? {
+        return ["Accept": "application/json"]
+    }
+
+    // Edit for .post, .update
+    func body() throws -> Data? {
+        return nil
+    }
 }
