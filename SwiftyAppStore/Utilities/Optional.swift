0a1037b38c8a005d18c421ceac0dce2ae6c2c699
Mico Infante <kinfante@isbx.com>
2022-07-31 12:10:03 +0800

    Easy unwrap optionals

---
+++ SwiftyAppStore/Utilities/Optional.swift
@@ -0,0 +1,28 @@
+//
+//  Optional.swift
+//  SwiftyAppStore
+//
+//  Created by Mico Infante on 7/31/22.
+//
+
+import Foundation
+
+protocol SomeOptional {
+    associatedtype Wrapped
+    func unwrap() throws -> Wrapped
+}
+
+struct ValueIsMissingError: Error {
+    var localizedDescription: String {
+        NSLocalizedString("No Data", comment: "")
+    }
+}
+
+extension Optional: SomeOptional {
+    func unwrap() throws -> Wrapped {
+        switch self {
+        case let .some(value): return value
+        case .none: throw ValueIsMissingError()
+        }
+    }
+}
