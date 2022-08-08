d12a951f16f31e8a1066e4a6b1ba076cefe864e8
Mico Infante <kinfante@isbx.com>
2022-08-08 20:25:31 +0800

    Pre-populate tab items

--- SwiftyAppStore/ContentView.swift
+++ SwiftyAppStore/ContentView.swift
@@ -10,10 +10,39 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            HomeView().preferredColorScheme(.dark).tabItem {
-                Label("Today", systemImage: "list.dash")
+            HomeView()
+                .preferredColorScheme(.dark)
+                .tabItem {
+                Label("Today", systemImage: "doc.text.image").foregroundColor(.primary)
             }
+
+            SomeView()
+                .tabItem {
+                    Label("Games", systemImage: "gamecontroller").foregroundColor(.primary)
+                }
+
+            SomeView()
+                .tabItem {
+                    Label("Apps", systemImage: "rectangle.on.rectangle").foregroundColor(.primary)
+                }
+
+            SomeView()
+                .tabItem {
+                    Label("Arcade", systemImage: "a.circle").foregroundColor(.primary)
+                }
+
+            SomeView()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass").foregroundColor(.primary)
+                }
+
         }
+    }
+}
+
+struct SomeView: View {
+    var body: some View {
+        Text("Some View")
     }
 }
 
