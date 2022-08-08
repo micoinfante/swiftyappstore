116b32975aef07d40919ea6f6277c9ab726265c7
Mico Infante <kinfante@isbx.com>
2022-08-08 20:00:20 +0800

    Add base api url and added networking constants

--- SwiftyAppStore/Resources/Constants.swift
+++ SwiftyAppStore/Resources/Constants.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 struct Constants {
+    private static var transportProtocol: String {
+        return "https"
+    }
     static var baseURL: String {
-        return "API_URL"
+        return "\(transportProtocol)://62ecc6df55d2bd170e868662.mockapi.io/api/v1"
+    }
+
+    struct Network {
+        static let MAX_RETRIES: Int = 3
+        static let MIN_TIME: Double = 0.5
+        static let TIME_OUT: Double = 15
     }
 }
